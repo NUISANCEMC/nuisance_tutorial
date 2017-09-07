bc577615679f961aa8289e3350cc99fa5ce4b6ca
patrickstowell <p.stowell@sheffield.ac.uk>
2017-09-07 09:07:58 -0500

    UPdated genie version

--- cvmfsbuild/nuisance-checkout.sh
+++ cvmfsbuild/nuisance-checkout.sh
@@ -5,5 +5,5 @@ mkdir builds
 cd builds
 mkdir genie2126-nuwrorw
 cd genie2126-nuwrorw
-cmake ../../ $NUISANCE_BUILD_FLAGS -DNO_EXPERIMENTS=1 -DNO_MINERvA=0
+cmake ../../ $NUISANCE_BUILD_FLAGS -DNO_EXPERIMENTS=1 -DNO_MINERvA=0 -DGENIE_VERSION=212
 make install
